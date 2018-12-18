b13d08ebc842eba6d8edb3e812b9d59e58885a16
PINAUD Frederic <frederic.pinaud@soprasteria.com>
2018-12-18 13:36:01 +0100

    Insert new user

--- react-native-insert.js
+++ react-native-insert.js
@@ -4,7 +4,7 @@ fetch('http://graphql.ddns.net/v1alpha1/graphql', {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
-    body: JSON.stringify({query: "{ user {lastname} }"})
-  })
+    body: JSON.stringify({query: 'mutation{ insert_user (objects: { firstname: "Lausenaz-Pire", lastname: "hugo", password: "TestPass"}){affected_rows}}'})
+})
     .then(r => r.json())
     .then(data => console.log('data returned:', data));
