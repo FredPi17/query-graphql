d0ae69afb15a2b0ca28f3c0b3d6a001ae517bbd3
PINAUD Frederic <frederic.pinaud@soprasteria.com>
2018-12-18 11:57:23 +0100

    main.js post to get data query working

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $.ajax({
     method: "POST",
-    url: "http://graphql.ddns.net/console/",
-    dataType: "JSON",
-    contentType: 'application/graphql',
-    data: '{ "user" { "lastname" } }',
+    url: "http://graphql.ddns.net/v1alpha1/graphql",
+    dataType: "json",
+    contentType: "application/json",
+    data: '{ "query": "{ user{ id lastname firstname } }" }',
     success: function(data) {
       console.log(data);
       //do something when request is successfull
